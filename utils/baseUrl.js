b0a047d8715f762374eacd6ff2258e60d5f0daed
Chris <christopherchartrand@gmail.com>
2021-04-20 09:45:22 -0400

    baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 // NODE_ENV is a node variable and lets us know if we're in development or production
 const baseUrl = process.env.NODE_ENV === "production" 
-    ? 'https://damaged-d2om28926-christopherchartrand.vercel.app'
+    ? 'https://damaged.vercel.app'
     : 'http://localhost:3000'
 
 export default baseUrl
