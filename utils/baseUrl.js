b59c1e5b250768eeb06a80c22ea06f2a58ccc2c8
Chris <christopherchartrand@gmail.com>
2021-04-20 09:19:54 -0400

    Updated production server baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,8 @@
 // NODE_ENV is a node variable and lets us know if we're in development or production
 const baseUrl = process.env.NODE_ENV === "production" 
-    ? 'https://deployment-url.now.sh'
+    ? 'https://damagedrecords.vercel.app'
     : 'http://localhost:3000'
 
 export default baseUrl
+
+damagedrecords.vercel.app
