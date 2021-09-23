c0d9417e4fae54b192ee6b41c5be3115f042b33c
christopher_ictinus <christopher@ictin.us>
2021-09-23 15:04:48 -0400

    styled artists and artist pages

--- pages/artists/index.js
+++ pages/artists/index.js
@@ -1,22 +1,30 @@
 import connectDb from '../../utils/connectDb'
 // import Artist from '../../models/Artist'
 import Link from 'next/link'
-import { Container, Segment, Image } from 'semantic-ui-react'
+// import { Container, Segment, Image } from 'semantic-ui-react'
 import axios from 'axios'
 import baseUrl from '../../utils/baseUrl'
+import globalStyles from '../../static/styles/global.module.scss'
+import artistsStyles from '../../static/styles/artists.module.scss'
 
 function Artists({ artists }) {
   
   return (
-      <div>
-        <h1>Artists</h1>
-        {artists.map(artist => (
-          <div key={artist._id}>
-            <Link href={`/artists/${artist.url}`}>{artist.name}</Link> 
-            <Link href={`/artists/${artist.url}`}><img src={artist.image}/></Link>
+    <div className={globalStyles.pageContainer}>
+      <div id={artistsStyles.artistsContainer}>
+        <div className={globalStyles.darkContainer}>
+          <div className={globalStyles.contentContainer}>
+            <h1 id={globalStyles.artistsh1}>Melted Roster (a-z)</h1>
+              {artists.map(artist => (
+              <div key={artist._id} >
+                <Link href={`/artists/${artist.url}`}><h2 id={globalStyles.artistsh2}>{artist.name}</h2></Link> 
+                <Link href={`/artists/${artist.url}`}><img src={artist.image} id={globalStyles.artistsImg} /></Link>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
+    </div>
   )
 }
 
