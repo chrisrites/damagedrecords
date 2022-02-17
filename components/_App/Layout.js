76af3963a6975fd9205b3f9b796244b96606371d
chrisrites <christopherchartrand@gmail.com>
2022-02-16 19:02:40 -0500

    tried fixing Poppins import

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -17,7 +17,7 @@ function Layout({ children, user }) {
         <HeadContent />
         {/* Stylesheets */}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@900&family=Montserrat&family=Merriweather&family=Noto+Sans+JP:wght@300&family=Rubik&family=Ubuntu:wght@400;500&family=Poppins:wght@0,400;0,500;0,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@900&family=Montserrat&family=Merriweather&family=Noto+Sans+JP:wght@300&family=Rubik&family=Ubuntu:wght@400;500&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://use.typekit.net/uqi8xbk.css" />
         <link
           rel="stylesheet"
