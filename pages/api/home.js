a3f2406774c17e2585e66a779f1906747b1e92ec
chrisrites <christopherchartrand@gmail.com>
2022-04-07 12:48:14 -0400

    server side env var log test

--- pages/api/home.js
+++ pages/api/home.js
@@ -20,6 +20,8 @@ export default async (req, res) => {
         yesterday.setDate(yesterday.getDate()-1)
         events = await Event.find({ date: { $gte: yesterday}  }).sort({date: 'asc'}).limit(3)
         // res.status(200).json({ artists, news, events })
+        console.log('PAYPAL ENV VAR: ' + process.env.REACT_APP_PAYPAL_CLIENT_ID)
+        console.log('CLOUDINARY ENV VAR: ' + process.env.CLOUDINARY_URL)
         res.status(200).json({ props: { artists, news, events }})
     } catch (error) {
         console.error(error)
