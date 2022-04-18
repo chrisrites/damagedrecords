79b5f1ba2bdc4044631a8071b92bdd7c74d52fea
chrisrites <christopherchartrand@gmail.com>
2022-04-18 12:04:16 -0400

    moved font cdns to fonts.scss

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,9 @@ export default function Document() {
     <Html>
       <Head>
         {/* Stylesheets */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@900&family=Montserrat&family=Merriweather&family=Noto+Sans+JP:wght@300&family=Rubik&family=Ubuntu:wght@400;500&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.typekit.net/uqi8xbk.css" />
+        
+        
+        
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
