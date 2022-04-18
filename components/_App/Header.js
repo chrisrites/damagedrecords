5e34e5d207a8dbcdf7f4ccfc28cd4489128b7b5e
chrisrites <christopherchartrand@gmail.com>
2022-04-18 10:08:15 -0400

    added password reset

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 // import { Menu, Container, Image, Icon } from 'semantic-ui-react';
-import { Image, Icon } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
---
+++ pages/api/passwordReset.js
@@ -0,0 +1,52 @@
+import connectDb from '../../utils/connectDb'
+import User from '../../models/User'
+import bcrypt from 'bcryptjs' 
+import shortid from 'shortid'
+// import isLength from 'validator/lib/isLength'
+// import jwt from 'jsonwebtoken'
+import sgMail from '@sendgrid/mail'
+
+sgMail.setApiKey(process.env.EMAIL_API_KEY);
+
+connectDb()
+
+export default async (req, res) => {
+    const { email } = req.body
+
+    try{
+        // Check if a user exists with the provided email.
+        const user = await User.findOne({ email })
+        // if not, return an error
+        if(!user){ 
+            return res.status(404).send('No user exists with that email address')
+        }
+        // if (!isLength(newPassword, { min:6 })) {
+        //     return res.status(422).send('Password must be at least 6 characters in length')
+        // }
+        
+        // const newPassword = 'test1234'
+        const newPassword = shortid.generate()
+
+        // hash their password
+        const hash = await bcrypt.hash(newPassword, 11)
+        // Create user
+        await User.findOneAndUpdate(
+            { email: email },
+            { password: hash }
+        )
+
+        const msg = {
+            to: 'christopherchartrand@gmail.com',
+            from: 'christopherchartrand@gmail.com',
+            subject: 'Password Changed',
+            name: 'Melted Wax Records',
+            // text: 'You have a new order!  Please ship asap and mark order as shipped',
+            html: '<h1>Password changed confirmation</h1><p>Your password has been changed to: ' + newPassword + '<br/><br/><p>Once logged in you can change your password in the Account page<br/><br/></p><img src="https://res.cloudinary.com/chrischartranddevelopment/image/upload/c_scale,w_228/v1647202668/epr6uehfm8cwacygsbuf.png"></img>'
+        };
+        await sgMail.send(msg);
+        res.status(200).send("Password changed successfully")
+    }catch(error){
+        console.error(error)
+        res.status(500).send('Error updating password')
+    }
+}
