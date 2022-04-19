d84233fce3cb25b9c279d83ee09dba9b78f1663a
chrisrites <christopherchartrand@gmail.com>
2022-04-19 09:06:49 -0400

    converting some env vars into public ones

--- pages/_app.js
+++ pages/_app.js
@@ -65,8 +65,8 @@ class MyApp extends App {
     return (
       <PayPalScriptProvider
         options={{ 
-          // "client-id": process.env.PAYPAL_CLIENT_ID,
-          "client-id": "AaHdCaMRG8nztlMTHgWLajyd5EHAiS_FpfPmSNE3BhQfCKcj5tmjljLW8ovVIJom0Kf8NkxJcFscWqEO",
+          "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+          // "client-id": "AaHdCaMRG8nztlMTHgWLajyd5EHAiS_FpfPmSNE3BhQfCKcj5tmjljLW8ovVIJom0Kf8NkxJcFscWqEO",
           "currency": "CAD"
         }}
       >
--- pages/admin.js
+++ pages/admin.js
@@ -28,7 +28,6 @@ function Admin({ user, orders}) {
     // Object.values returns an array of the values in our product object.  Then the every function goes through each value and we're using the Boolean function to check if they have values (if they're truthy)
     const isProduct = Object.values(product).every(el => Boolean(el))
     isProduct ? setDisabled(false) : setDisabled(true)
-    console.log("CLOUDINARY ENV: " + process.env.CLOUDINARY_URL)
   }, [product])
 
   function handleChange(event){
@@ -49,7 +48,7 @@ function Admin({ user, orders}) {
     data.append('file', product.media)
     data.append('upload_preset', 'damaged')
     data.append('cloud_name', 'chrischartranddevelopment')
-    const response = await axios.post(process.env.CLOUDINARY_URL, data)
+    const response = await axios.post(process.env.NEXT_PUBLIC_CLOUDINARY_URL, data)
     const mediaUrl = response.data.url
     return mediaUrl
   }
